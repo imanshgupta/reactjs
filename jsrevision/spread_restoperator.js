d5e2cb38c80bcf1aa1da2884b03fda65f1b11ae4
ANSH <anshgupta.mca20@gmail.com>
2021-05-14 21:54:34 +0530

    rest operator

--- jsrevision/spread_restoperator.js
+++ jsrevision/spread_restoperator.js
@@ -10,4 +10,11 @@ let newobj={
     mind="jenius"//here thsi new object has all the members of the old object of name and age
 
 }
-//---------------------rest operator
+//---------------------rest operator------------------------------
+//used to merge a lot of arguments into the function as an array 
+
+hellow=(...arg) => arg.sort();
+//function hellow(..args){
+// return arg.sort();
+//this arg is an array that consists of all the parameters passed inside the function
+// }
