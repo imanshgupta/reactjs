0dd03752e6e1f0db5de8ae48d45554b5650b8a66
ANSH <anshgupta.mca20@gmail.com>
2021-05-14 22:13:56 +0530

    aliasing array and objects

---
+++ jsrevision/referenceandprimitive.js
@@ -0,0 +1,10 @@
+var num = 1;
+//number strings boolean can be copied as they are pri,itive types imp
+num1 = num;
+//here if we change num1 there is no change in num but in arrays its aliasing
+var arr = [1, 2, 3, 4, 4, 5];
+var c = arr;
+c[1] = 33;
+console.log(arr);
+//after this in arr at index 1 value is changed so basically c points to arr
+//somilar is with objects so we cannot make copies of array  or objects like this when working in react
