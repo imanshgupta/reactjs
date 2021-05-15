34710e83cf2cdbae944cba3fc00cb7a0380394c5
ANSH <anshgupta.mca20@gmail.com>
2021-05-16 00:12:22 +0530

    basic changes

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,11 +4,16 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "DINNER",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "GIFT GIRLFRIEND",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
       title: "Car Insurance",
