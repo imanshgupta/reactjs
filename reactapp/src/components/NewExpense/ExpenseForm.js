7eb0ba8535a8866e82451ff694285442fe0ed51d
ANSH <anshgupta.mca20@gmail.com>
2021-05-17 04:04:17 +0530

    setting up states for submit

--- reactapp/src/components/NewExpense/ExpenseForm.js
+++ reactapp/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
+  const [changedtitle, changetitle] = useState("");
+  const [changedamount, changeamount] = useState(0);
+  const [changeddate, changedate] = useState();
+  const titlechangeHandler = (event) => {
+    changetitle(event.target.value);
+  };
+  const changeamountHander = (event) => {
+    changeamount(event.target.value);
+  };
+  const changedateHandeller = (event) => {
+    changedate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titlechangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="200" step="100" />
+          <input
+            type="number"
+            min="200"
+            step="100"
+            onChange={changeamountHander}
+          />
         </div>
         <div className="new-expense__control">
-          <label>Title</label>
-          <input type="date" min="2021-01-01" max="22-01-01" />
+          <label>Date</label>
+          <input
+            type="date"
+            min="2021-01-01"
+            max="22-01-01"
+            onChange={changedateHandeller}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
