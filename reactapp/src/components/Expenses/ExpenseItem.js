60397838c620d4b4a140e745e979ca949f2b77af
ANSH <anshgupta.mca20@gmail.com>
2021-05-17 02:03:30 +0530

    added event handellers

--- reactapp/src/components/Expenses/ExpenseItem.js
+++ reactapp/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const eventHandeller = () => {};
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">Rs {props.amount}</div>
+        <button onClick={eventHandeller}>BUTTON FOR EVENT</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
